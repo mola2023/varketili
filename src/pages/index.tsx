ae4236747664608cd59930b42def4ef6976bd092
hedysnika <arebanikko@gmail.com>
2023-02-27 18:34:11 -0800

    eagaeg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,10 @@ export default function Index(){
     <>
       <main className="bg-black h-screen text-white">
         <div className="text-center pt-10 text-7xl">
+<div className="flex justify-center items-center gap-10">
 
-        platos tochkebii
+        platos tochkebii <img src="https://i.imgur.com/aDH2dkT.png" alt="" />
+</div>
         </div>
 <div className="text-center mt-40 bg-[#f5ec42] text-black border border-white py-2 mx-5">plato mdebareobs ukiduresi varketilit. aq cxovroben shariani mastebi da zogjer ichitebian dedamotynulebic romlebsac shoridanve tasaobis manerit, andac saubris stilit daumugamebt.
 aseti mastebis gatyvnistanave unda gaacno tavi da tu iazvobs deda unda moutynat. 
