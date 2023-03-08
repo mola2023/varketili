61d45c147bb0cc3770ccbbc2b42d2f0cafb80722
hedysnika <arebanikko@gmail.com>
2023-03-07 18:10:43 -0800

    aegaeg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,22 @@ export default function Index(){
     
     return (
     <>
-      <main className="bg-black min-h-screen h-auto text-white pb-52">
+      <main className="bg-[#242424] min-h-screen h-auto text-white pb-52">
         <div className="text-center pt-10 text-7xl">
-<div className="md:flex justify-center md:items-center gap-10">
+<div className="flex justify-center md:items-center md:gap-10">
+<img className="md:h-[120px] w-[80px] h-[80px] md:w-[120px] md:flex md:justify-center" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Vorovskaya-zvezda-aue.svg/2048px-Vorovskaya-zvezda-aue.svg.png"/>
+<div className="pb-7 hover:text-[#000000] font-semibold"> პლატო </div>
+<img className="md:h-[120px] w-[80px] h-[80px] md:w-[120px] md:flex md:justify-center" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Vorovskaya-zvezda-aue.svg/2048px-Vorovskaya-zvezda-aue.svg.png"/>
 
-        platos tochkebii <img src="https://i.imgur.com/aDH2dkT.png" alt="" />
 </div>
         </div>
-<div className="text-center mt-40 bg-[#f5ec42] text-black border border-white py-2 mx-5">plato mdebareobs ukiduresi varketilit. aq cxovroben shariani mastebi da zogjer ichitebian dedamotynulebic romlebsac shoridanve tasaobis manerit, andac saubris stilit daumugamebt.
-aseti mastebis gatyvnistanave unda gaacno tavi da tu iazvobs deda unda moutynat. 
-  
-</div>
+<div className=" mt-[40px] text-black border border-white my-2 mx-auto w-52"> </div>
+
+
+<div className="fixed bottom-0 text-center w-full h-[35px]">Developed by Giorgi Molashvili © 2023</div>
+
       </main>
     </>
   );
 }
+
